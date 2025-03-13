bee9d95f95e11019c574ae1ffc91d9c1b0ece308
Goksel Goktas <gokselgoktas@users.noreply.github.com>
2025-03-13 23:31:33 +0000

    Add ScreenSpaceVertexSnapping

---
+++ Assets/Shaders/VertexSnapping.hlsl
@@ -0,0 +1,9 @@
+void ApplyScreenSpaceVertexSnapping_float(in float4 IN_positionSS, out float3 OUT_positionSS)
+{
+    float3 positionSS = float3(IN_positionSS.xy / IN_positionSS.w, IN_positionSS.z);
+    float2 resolution = _ScreenParams.xy * rcp(_Accuracy);
+
+    positionSS.xy = floor(positionSS.xy * resolution) * rcp(resolution);
+
+    OUT_positionSS = positionSS;
+}
