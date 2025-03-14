362f8b7f21ef7620b53953a25060fbae04091a08
Goksel Goktas <gokselgoktas@users.noreply.github.com>
2025-03-14 08:10:34 +0000

    Rename shader graph properties

--- Assets/Shaders/VertexSnapping.hlsl
+++ Assets/Shaders/VertexSnapping.hlsl
@@ -1,7 +1,7 @@
 void ApplyScreenSpaceVertexSnapping_float(in float4 IN_positionSS, out float3 OUT_positionSS)
 {
     float3 positionSS = float3(IN_positionSS.xy / IN_positionSS.w, IN_positionSS.z);
-    float2 resolution = _ScreenParams.xy * rcp(_Accuracy);
+    float2 resolution = _ScreenParams.xy * rcp(_ScreenSpaceQuantizationFactor);
 
     positionSS.xy = floor(positionSS.xy * resolution) * rcp(resolution);
 
