def67fad6577b70942682ca5e15189a308b3f850
Goksel Goktas <gokselgoktas@users.noreply.github.com>
2025-05-11 13:41:26 +0100

    Upgrade to Unity 6.1

--- Assets/Scripts/VertexSnapping.cs
+++ Assets/Scripts/VertexSnapping.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class VertexSnapping : MonoBehaviour
 {
     [Range(1, 16)]
-    public int screenSpaceQuantizationFactor = 4;
+    public int ScreenSpaceQuantizationFactor = 4;
 
     public void OnValidate()
     {
-        Shader.SetGlobalFloat("_ScreenSpaceQuantizationFactor", (float)screenSpaceQuantizationFactor);
+        Shader.SetGlobalFloat("_ScreenSpaceQuantizationFactor", (float)ScreenSpaceQuantizationFactor);
     }
 }
